38a8db980c85415df6a302bfc391dc15f7c50b90
ald1ndc <aldindc@yahoo.com>
2024-08-20 21:30:55 +0800

    first cloud native v4

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.send("Hello World AldinDC");
+});
+
+app.post('/', function (req, res) {
+  res.send("Hi AldinDC");
 });
 
 app.listen(3000);
